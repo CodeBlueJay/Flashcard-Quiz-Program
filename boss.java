ea772dc7b98e2571f035bc9ccb8af277edbb7ad1
CodeBlueJay <codebluejay12@gmail.com>
2025-10-31 23:38:33 -0700

    Initialized Boss class

--- boss.java
+++ boss.java
@@ -1,2 +1,15 @@
 public class Boss {
+    private int health;
+    private int bossHealth;
+    private String[] words;
+    private String[] meanings;
+    private boolean mcq;
+
+    public Boss(String[] w, String[] m, boolean isMcq) {
+        this.health = 5;
+        this.bossHealth = 5;
+        this.words = w;
+        this.meanings = m;
+        this.mcq = isMcq;
+    }
 }
