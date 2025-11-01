45c52e6c85b008d913721b94e32fea53a25d985c
CodeBlueJay <codebluejay12@gmail.com>
2025-10-31 23:20:49 -0700

    Fixed variable declaration in Learn class

--- learn.java
+++ learn.java
@@ -14,10 +14,11 @@ public class Learn {
     public String weightedChoice(String[] items, double[] weights) {
         double totalWeight = 0.0;
         Random rand = new Random();
-        double randomValue = rand.nextDouble() * totalWeight;
+        double randomValue;
         for (double weight : weights) {
             totalWeight += weight;
         }
+        randomValue = rand.nextDouble() * totalWeight;
         for (int i = 0; i < items.length; i++) {
             randomValue -= weights[i];
             if (randomValue <= 0.0) {
