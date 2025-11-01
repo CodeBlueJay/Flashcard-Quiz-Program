74d1a6bb36afe2bb85933e9a582cd841ea6869fd
CodeBlueJay <codebluejay12@gmail.com>
2025-10-31 23:18:27 -0700

    Added weightedChoice to Learn class

--- learn.java
+++ learn.java
@@ -1,2 +1,29 @@
+import java.util.Random;
+
 public class Learn {
+    private boolean text;
+    private String[] words;
+    private double[] weights;
+
+    public Learn(boolean t, String[] w, double[] we) {
+        this.text = t;
+        this.words = w;
+        this.weights = we;
+    }
+
+    public String weightedChoice(String[] items, double[] weights) {
+        double totalWeight = 0.0;
+        Random rand = new Random();
+        double randomValue = rand.nextDouble() * totalWeight;
+        for (double weight : weights) {
+            totalWeight += weight;
+        }
+        for (int i = 0; i < items.length; i++) {
+            randomValue -= weights[i];
+            if (randomValue <= 0.0) {
+                return items[i];
+            }
+        }
+        return null;
+    }
 }
