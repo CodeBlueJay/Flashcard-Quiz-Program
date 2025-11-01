b04455a07a36b5787886515765e181200a49eb0f
CodeBlueJay <codebluejay12@gmail.com>
2025-10-31 23:23:11 -0700

    test

---
+++ push.bat
@@ -0,0 +1,14 @@
+@echo off
+set /p msg=Enter commit message: 
+echo.
+echo Adding all files
+git add --all
+echo.
+echo Committing with message: "%msg%"
+git commit -m "%msg%"
+echo.
+echo Pushing to repository
+git push
+echo.
+echo Synced
+pause
