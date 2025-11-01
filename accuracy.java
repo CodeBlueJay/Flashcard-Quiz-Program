dff3aef11f8ea3a2cf10ea1779bc83f97e9b46ed
CodeBlueJay <codebluejay12@gmail.com>
2025-10-31 23:40:07 -0700

    Initialized Accuracy class

--- accuracy.java
+++ accuracy.java
@@ -1,2 +1,13 @@
 public class Accuracy {
+    private String[] words;
+    private String[] meanings;
+    private int questionsCorrect;
+    private double time;
+
+    public Accuracy(String[] w, String[] m) {
+        this.words = w;
+        this.meanings = m;
+        this.questionsCorrect = 0;
+        this.time = 7.0;
+    }
 }
