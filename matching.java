22ec35ba8b91cdf2b47c6cec9ce568d40955e92b
CodeBlueJay <codebluejay12@gmail.com>
2025-10-31 23:27:47 -0700

    Initialized Matching class

--- matching.java
+++ matching.java
@@ -1,2 +1,9 @@
 public class Matching {
+    private String[] words;
+    private String[] meanings;
+
+    public Matching(String[] w, String[] m) {
+        this.words = w;
+        this.meanings = m;
+    }
 }
