af51d19e0539a713fed508fc57ad6e6deeb0b21f
Jayden Ratanatharathorn <800025364@fjuhsd.org>
2025-11-13 14:22:22 -0800

    Accuracy class stuff

--- Accuracy.java
+++ Accuracy.java
@@ -16,6 +16,11 @@ public class Accuracy extends VBox {
     private int questionsCorrect;
     private double time;
     private EXPBarUI expBar;
+    // more gui components
+    private Label accuracylabel = new Label("Accuracy");
+    private HBox container = new HBox(8);
+    private TextField answer = new TextField();
+    private Button submit = new Button("Submit");
 
     public Accuracy(ArrayList<String> w, ArrayList<String> m, ArrayList<Double> we, EXPBarUI exp) {
         words = w;
@@ -24,7 +29,25 @@ public class Accuracy extends VBox {
         questionsCorrect = 0;
         time = 7.0;
         expBar = exp;
+        setSpacing(10);
+        setPadding(new Insets(16));
+        answer.getStyleClass().add("answer");
+        container.getChildren().addAll(accuracylabel, container, answer, submit);
+        getChildren().add(container);
+
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                String ans = "";
+                if (!(answer.getText() == null)) {
+                    ans = answer.getText();
+                } else {
+                    ans = "";
+                }
+                answer.setText(ans);
+            }
+        });
     }
 
-
+    
 }
