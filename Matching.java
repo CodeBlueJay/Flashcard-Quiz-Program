3dc057b8e992d065bc2e2b45eb71fdffa1405a69
Ahmad Yahia <800025494@fjuhsd.org>
2025-11-17 14:25:01 -0800

    Created a commented-out to do list for Matching class

--- Matching.java
+++ Matching.java
@@ -6,4 +6,16 @@ public class Matching {
         this.words = w;
         this.meanings = m;
     }
+
+    /** Needs to acount for: 
+     * Creating buttons
+     * Actions that occur on button click
+     * Setting buttons equal to term / definition
+     * Setting button pairs (one term on the left side that corresponds with term on the right side)
+     * Removal of buttons that were clicked
+     * 
+     * Match action (clicking one button, then clicking another on the other side, without issue occuring)
+     * Check action (Checking to see if buttons that were clicked are a correct pair)
+     * */ 
+
 }
