7a41447484655a8c1fba7797fc4dd73d3f8ea730
Jayden Ratanatharathorn <800025364@fjuhsd.org>
2025-11-21 13:46:44 -0800

    Added .sh equivalent scripts for .bat files for Linux/Mac environments

---
+++ push.sh
@@ -0,0 +1,21 @@
+@echo off
+git config core.ignorecase false
+echo Updating local branch
+git pull
+echo.
+set /p msg=Enter description of changes: 
+if "%msg%"=="" (
+    echo Commit message cannot be empty.
+    goto :eof
+) 
+echo.
+echo Adding all files
+git add --all
+echo.
+echo Committing with message: "%msg%"
+git commit -m "%msg%"
+echo.
+echo Pushing to repository
+git push
+echo.
+echo Synced
